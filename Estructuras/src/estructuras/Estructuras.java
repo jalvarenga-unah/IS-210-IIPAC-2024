763045246203cd146f72b3b3f54dd844e3bf4e93
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-07-02 20:03:07 -0600

    practica de clase

--- Estructuras/src/estructuras/Estructuras.java
+++ Estructuras/src/estructuras/Estructuras.java
@@ -30,13 +30,52 @@ public class Estructuras {
         System.out.println(nombre + " " + apellido);
         System.out.println(persona);
 
+        HashMap[] usuarios = new HashMap[3];
+
         HashMap<String, String> credenciales = new HashMap();
         credenciales.put("usuario", "admin");
         credenciales.put("contrasenia", "1234");
 
-        String user = "admin";
-        String pass = "1234";
+        HashMap<String, String> credenciales2 = new HashMap();
+        credenciales2.put("usuario", "caja");
+        credenciales2.put("contrasenia", "1122");
 
+        HashMap<String, String> credenciales3 = new HashMap();
+        credenciales3.put("usuario", "reportes");
+        credenciales3.put("contrasenia", "4321");
+
+        usuarios[0] = credenciales;
+        usuarios[1] = credenciales2;
+        usuarios[2] = credenciales3;
+
+        String user = "caja";
+        String pass = "1122";
+
+//        System.out.println(usuarios[0]); // credenciales
+        boolean encontrado = false;
+        for (int i = 0; i < usuarios.length; i++) {
+
+            if (user.equals(usuarios[i].get("usuario"))
+                    && pass.equals(usuarios[i].get("contrasenia"))) {
+                encontrado = true;
+                break;
+            }
+
+        }
+
+        if (encontrado ) {
+            System.out.println("Bienvenido");
+        } else {
+            System.out.println("Verifique los datos");
+        }
+
+        /*
+        [
+            {usuario=admin, contrasenia=1234},
+            {usuario=caja, contrasenia=1122},
+            {usuario=reportes, contrasenia=4321}
+        ]
+         */
 //        String a = "a"; // instancias
 //        String b = "b";
 //        String c = a + b;
@@ -49,14 +88,13 @@ public class Estructuras {
 //        } else {
 //            System.out.println("ooops!! NO SE CUMPLE!!!");
 //        }
-
-        if (user.equals(credenciales.get("usuario")) && 
-                pass.equals(credenciales.get("contrasenia"))) {
-            System.out.println("Bienvenido");
-        
-        }else {
-            System.out.println("Verifique los datos");
-        }
+//        if (user.equals(credenciales.get("usuario"))
+//                && pass.equals(credenciales.get("contrasenia"))) {
+//            System.out.println("Bienvenido");
+//
+//        } else {
+//            System.out.println("Verifique los datos");
+//        }
     }
 
 }
